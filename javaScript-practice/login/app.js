4d32ec9cc25a157828700f8a6d2287a29c8113b1
jaemin <sonjm3518@dsm.hs.kr>
2022-06-20 16:07:21 +0900

    ⚡ :: const로 string 고정시키기

--- javaScript-practice/login/app.js
+++ javaScript-practice/login/app.js
@@ -2,11 +2,19 @@ const login = document.querySelector(".login");
 const loginInput = login.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
+const ACCOUNTID_KEY = "accountId";
+
 function onLoginButtonClick(event) {
     event.preventDefault();
     const accountId = loginInput.value;
-    localStorage.setItem("accountId", accountId);
+    localStorage.setItem(ACCOUNTID_KEY, accountId);
     greeting.innerText = `${accountId}`;
 }
 
 login.addEventListener("submit", onLoginButtonClick);
+
+const savedAccountId = localStorage.getItem(ACCOUNTID_KEY);
+
+if(savedAccountId == null) {
+    console.log("404");
+}
