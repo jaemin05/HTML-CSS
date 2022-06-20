8c903e7ec9c35502bcc296660a00fb626884ebed
jaemin <sonjm3518@dsm.hs.kr>
2022-06-20 21:44:24 +0900

    📑 :: clock js

---
+++ javaScript-practice/clock/app.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector("h1#clock");
+
+clock.innerText = "lalala"
+
+function getDate() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0")
+    const minutes = String(date.getMinutes()).padStart(2, "0")
+    const seconds = String(date.getSeconds()).padStart(2, "0")
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getDate();
+setInterval(getDate, 1000);
